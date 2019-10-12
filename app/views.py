1e4e72d49d2874fdde7f19cb22278c0be7c4cd36
Tyra-hans <tyrahans17@gmail.com>
2019-10-12 17:55:07 +0300

    adds news source page route that allows us to route to the news source page and acces the data

---
+++ app/views.py
@@ -0,0 +1,19 @@
+from flask import render_template
+from app import app
+
+#Views
+@app.route('/')
+def index():
+    '''
+    View root page function that returns the index page and it's data
+    '''
+    message = 'Hello Tyra'
+    return render_template('index.html', message = message)
+
+@app.route('/news/<int:news_source_id>')
+def news_source(news_source_id):
+
+    '''
+    View news source page function that returns the news sources page and it's data
+    '''
+    return render_template('newssource.html, id = news_source_id')
