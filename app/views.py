36f5d631ac81c9c69bdec78a83c447eea6fa7c0b
Tyra-hans <tyrahans17@gmail.com>
2019-10-12 19:52:17 +0300

    creates instance directory that has the config.py file where the api key is placed.places directory in gitignore file and connects the application with the configuration file

--- app/views.py
+++ app/views.py
@@ -7,8 +7,9 @@ def index():
     '''
     View root page function that returns the index page and it's data
     '''
-    message = 'Hello Tyra'
-    return render_template('index.html', message = message)
+    
+    title = 'Home - Welcome to The Know,News from any place, anytime!'
+    return render_template('index.html', title = title)
 
 @app.route('/news/<int:news_source_id>')
 def news_source(news_source_id):
