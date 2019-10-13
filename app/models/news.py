879ccd8fdbea7ca0951e6a7b630e3951676ac688
Tyra-hans <tyrahans17@gmail.com>
2019-10-13 12:33:59 +0300

    creates and runs 1 test on the correct instantiation of the news object to show that we are able to make news instances

--- app/models/news.py
+++ app/models/news.py
@@ -6,7 +6,7 @@ class News:
     def __init__(self,name,title,description,url,urlToImage,publishedAt,content):
         self.name = name
         self.title = title
-        self.description = desciption
+        self.description = description
         self.url = "https://www.youtube.com/watch?v=RN75zSpYp7M"
         self.urlToImage = "https://i.ytimg.com/vi/RN75zSpYp7M/maxresdefault.jpg"
         self.publishedAt = publishedAt
--- app/news_test.py
+++ app/news_test.py
@@ -11,4 +11,12 @@ class NewsTest(unittest.TestCase):
         '''
         Set up method that will run before every test
         '''
-        self.new_news = News(1234,'This is what all the fuss is about ?','I can handle that','')
+        self.new_news = News('this is the name','This is the title','description','https://www.youtube.com/watch?v=RN75zSpYp7M','https://i.ytimg.com/vi/RN75zSpYp7M/maxresdefault.jpg',"2019-10-13T07:04:27Z","Find out what Joanna Jedrzejczyk had to say backstage after her main event performance at UFC Tampa!\r\nSubscribe to get all the latest UFC content: http://bit.ly/2uJRzRR\r\nExperience UFC live with UFC FIGHT PASS, the digital subscription service of the UFC. Vis… [+491 chars]")
+
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_news,News))
+
+
+if __name__ == '__main__':
+    unittest.main()    
