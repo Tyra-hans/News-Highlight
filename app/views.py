3429cbefaba522e2b61aa69be3648b8e34ff6bc5
Tyra-hans <tyrahans17@gmail.com>
2019-10-13 16:01:30 +0300

    displays the top headlines and the content by consuming the news api

--- app/views.py
+++ app/views.py
@@ -8,11 +8,12 @@ def index():
     '''
     View root page function that returns the index page and it's data
     '''
-    #getting trending news
+    # getting trending news
     popular_news = get_news('top-headlines')
     print(popular_news)
+    
     title = 'Home - Welcome to The Know,News from any place, anytime!'
-    return render_template('index.html', title = title,popular = popular_news)
+    return render_template('index.html', title = title, popular = popular_news)
 
 @app.route('/news/<news_source_name>')
 def news_source(news_source_name):
