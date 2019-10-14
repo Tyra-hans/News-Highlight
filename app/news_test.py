9f51106dcf69a71e0b26f0901ffd2df013b51e7a
Tyra-hans <tyrahans17@gmail.com>
2019-10-14 12:52:56 +0300

    creates a test for the correct instantiation of the Articles class to ensure that Articles class is properly instantiated

--- app/news_test.py
+++ app/news_test.py
@@ -11,7 +11,7 @@ class NewsTest(unittest.TestCase):
         '''
         Set up method that will run before every test
         '''
-        self.new_news = News('this is the name','This is the title','description','https://www.youtube.com/watch?v=RN75zSpYp7M','https://i.ytimg.com/vi/RN75zSpYp7M/maxresdefault.jpg',"2019-10-13T07:04:27Z","Find out what Joanna Jedrzejczyk had to say backstage after her main event performance at UFC Tampa!\r\nSubscribe to get all the latest UFC content: http://bit.ly/2uJRzRR\r\nExperience UFC live with UFC FIGHT PASS, the digital subscription service of the UFC. Vis… [+491 chars]")
+        self.new_news = News('id','This is the name','description','https://www.youtube.com/watch?v=RN75zSpYp7M','category','kenya')
 
 
     def test_instance(self):
